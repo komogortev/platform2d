de46731c1e8514bc3a21ee1d9071086ee180336e
komogortev <komogortev@gmail.com>
2018-05-16 23:54:32 -0400

    character room decoration
    added basic versions of warderobe, table and mannequins sprites

--- Assets/Scripts/LevelsManager.cs
+++ Assets/Scripts/LevelsManager.cs
@@ -8,6 +8,7 @@ public class LevelsManager : MonoBehaviour {
 
 	//	public GameObject loadingImage;
 	public Text totalScoreCount;
+	private bool isMenu = true;
 
 	void Start () {
 		if (SceneManager.GetActiveScene().name == "_Main") {
@@ -15,12 +16,54 @@ public class LevelsManager : MonoBehaviour {
 			Debug.Log ("AA" + totalScoreCount.text);
 		}
 	}
+	  
 
 	public void LoadScene(string level){
 		//loadingImage.SetActive (true);
 		SceneManager.LoadScene (level);
 	}
 		
+
+
+	//Toggle to Character room
+	public void ToggleRoom () {
+		isMenu = !isMenu;
+
+		//find objects that move
+		GameObject menuScene = GameObject.Find ("MenuScene");
+		GameObject scenesNav = GameObject.Find ("ScenesNav");
+		//set menu positions
+		Vector3 menuMarker = new Vector3 (0, 0, 0);
+		Vector3 roomMarker = new Vector3 (0, 10, 0);
+		//@Todo init canvas values for nav
+		Vector3 navMenu = new Vector3 (309.5f, 174f, 0f);
+		Vector3 navRoom = new Vector3 (309.5f, 664f, 0f);
+
+		//slide menu/room to camera view position
+		StartCoroutine(SlideMenu(menuScene, isMenu == true ? menuMarker : roomMarker, .2f));
+		StartCoroutine(SlideMenu(scenesNav, isMenu == true ? navMenu : navRoom, .2f));
+	}
+
+
+	private IEnumerator SlideMenu(GameObject scene, Vector3 location, float delta) {
+		float closeEnough = .2f;
+		float distance = (scene.transform.position - location).magnitude;
+		  
+		while (distance >= closeEnough) {
+			scene.transform.position = Vector3.Slerp (scene.transform.position, location, delta);
+			yield return new WaitForEndOfFrame ();
+			//Update distance to final location;	
+			distance = (scene.transform.position -	 location).magnitude;
+		}
+
+		//final moving touch from closeEnough to final location
+		scene.transform.position = location;
+
+	}
+
+
+
+
 	public void Quit(){
 		Application.Quit ();
 	}
